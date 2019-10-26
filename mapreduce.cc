042914a6fa1df3c87352667dc5b4708194cdd636
kenboo98 <tellambu@ualberta.ca>
2019-10-26 17:50:53 -0600

    Added stuff

--- mapreduce.cc
+++ mapreduce.cc
@@ -13,15 +13,28 @@
 
 using namespace std;
 
-vector<multimap<string, int>> partitions;
-int global_n_partitions;
+typedef struct Partitions_t{
+    vector<multimap<string, int>> partitions;
+    vector<pthread_mutex_t> partition_lock;
+    int n_partitions;
+    Partitions_t(int partitions){
+        this->n_partitions = partitions;
+        for(int i = 0; i<partitions; i++){
+            pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+            this->partition_lock.push_back(mutex);
 
-bool job_comparator(string file1, string file2){
+        }
+    }
+} Partitions_t;
+
+
+bool job_comparator(string file1, string file2) {
     struct stat stat1, stat2;
     stat(file1.c_str(), &stat1);
     stat(file2.c_str(), &stat2);
     return (stat1.st_size < stat2.st_size);
 }
+
 /* Main Program in MapReduce. Passed an array of file names,
  * number of reducers and mappers, and function pointers to the
  * map and reduce functions.
@@ -33,23 +46,27 @@ bool job_comparator(string file1, string file2){
 
 void MR_Run(int num_files, char *filenames[],
             Mapper map, int num_mappers,
-            Reducer concate, int num_reducers){
+            Reducer concate, int num_reducers) {
+    Partitions_t *partitions = new Partitions_t();
+    partitions-> n_partitions = num_reducers;
+
     // Create the reducer threads
     ThreadPool_t *tp = ThreadPool_create(num_mappers);
     vector<string> jobs;
-    for(int i=0; i< num_files; i++){
+    for (int i = 0; i < num_files; i++) {
         string file = filenames[i];
         jobs.push_back(file);
     }
     //sort jobs by length of file
     sort(jobs.begin(), jobs.end(), job_comparator);
-    for(int i=0; i< num_files; i++){
+    for (int i = 0; i < num_files; i++) {
         ThreadPool_add_work(tp, (thread_func_t) map, (void *) jobs[i].c_str());
     }
     sleep(5);
 
 
 }
+
 /* Called by a mapper thread. Determines which partition to write k/v
  * pair from the MR_Partition function. MR_Emit then inserts the pair
  * in the correct partition, while keeping the partition sorted.
@@ -57,28 +74,31 @@ void MR_Run(int num_files, char *filenames[],
  */
 void MR_Emit(char *key, char *value) {
 }
+
 /* Takes a key and the number of partitions and returns
  * the partition that a particular key must be in
  *
  */
-unsigned long MR_Partition(char *key, int num_partitions){
+unsigned long MR_Partition(char *key, int num_partitions) {
     unsigned long hash = 5381;
     int c;
     while ((c = *key++) != '\0')
         hash = hash * 33 + c;
     return hash % num_partitions;
 }
+
 /* Process Partition is called on each partition by each
  * Reducer thread. This function invokes user defined
  * Reduce function on the next unprocessed key.
  *
  */
-void MR_ProcessPartition(int partition_number){
+void MR_ProcessPartition(int partition_number) {
 
 }
+
 /* Returns the next value associated with the given key in the
  * sorted partition or Null when it has been completely processed
  */
-char *MR_GetNext(char *key, int partition_number){
+char *MR_GetNext(char *key, int partition_number) {
 
 }
